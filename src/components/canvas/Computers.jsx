3ffe0ba96c74522bcc80600b27ade535d6e7d101
MasonGuinn <mason_guinn@outlook.com>
2023-09-22 03:51:09 -0700

    0.1.5

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
+import { extend } from '@react-three/fiber';
+extend({ OrbitControls });
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+	const computer = useGLTF('./desktop_pc/scene.gltf');
+	return (
+		<mesh>
+			<directionalLight intensity={1} position={[-20, 50, 10]} />
+			<ambientLight intensity={1} />
+			<hemisphereLight intensity={1} groundColor='black' />
+			<pointLight intensity={1} />
+			<primitive
+				position={[0, -3.25, -1.5]}
+				rotation={[-0.01, -0.2, -0.1]}
+				scale={0.75}
+				object={computer.scene}
+			/>
+		</mesh>
+	);
+};
 
-export default Computers
+const ComputersCanvas = () => {
+	return (
+		<Canvas
+			frameloop='demand'
+			shadows
+			camera={{ position: [20, 3, 5], fov: 25 }}
+			gl={{ preserveDrawingBuffer: true }}>
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls
+					enableZoom={false}
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+				/>
+				<Computers />
+			</Suspense>
+			<Preload all />
+		</Canvas>
+	);
+};
+
+export default ComputersCanvas;
