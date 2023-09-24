49cea63072742913ef34c505b61f5985f9ec56c1
MasonGuinn <mason_guinn@outlook.com>
2023-09-23 19:23:32 -0700

    0.1.7

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/no-unknown-property */
-import * as THREE from 'three';
-import React, { useState, useEffect } from 'react';
+import * as THREE from 'three'; // Import the THREE.js library
+import React, { useState, useEffect } from 'react'; // Import necessary React components and hooks
 import { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import CanvasLoader from '../Loader';
-import { extend } from '@react-three/fiber';
-extend({ OrbitControls });
+import { Canvas } from '@react-three/fiber'; // Import components from react-three/fiber
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'; // Import components from react-three/drei
+import CanvasLoader from '../Loader'; // Import a custom component
+import { extend } from '@react-three/fiber'; // Import extend function
+extend({ OrbitControls }); // Extend the capabilities of the OrbitControls component
 
+// Component for rendering the 3D model of a computer
 const Computers = ({ isMobile }) => {
+	// Load the 3D model of a computer using useGLTF hook
 	const computer = useGLTF('./desktop_pc/scene.gltf');
+
+	// Return a mesh containing various lights and the 3D computer model
 	return (
 		<mesh>
 			<directionalLight
@@ -23,47 +27,61 @@ const Computers = ({ isMobile }) => {
 			/>
 			<pointLight intensity={1} />
 			<primitive
-				position={[0, -3.25, -1.5]} // Corrected position syntax
-				rotation={[-0.01, -0.2, -0.1]}
-				scale={isMobile ? 0.7 : 0.75}
-				object={computer.scene}
+				position={isMobile ? [0, -2, -1.5] : [0, -3.25, -1.5]} // Set the position based on whether it's mobile or not
+				rotation={[-0.01, -0.2, -0.1]} // Set the rotation
+				scale={isMobile ? 0.6 : 0.75} // Set the scale based on whether it's mobile or not
+				object={computer.scene} // Set the 3D model to be rendered
 			/>
 		</mesh>
 	);
 };
 
+// Main component for rendering the 3D canvas
 const ComputersCanvas = () => {
-	const [isMobile, setIsMobile] = useState(false);
+	const [isMobile, setIsMobile] = useState(false); // Initialize a state variable for mobile detection
 
+	// Use the useEffect hook to add a listener for changes to the screen size
 	useEffect(() => {
-		const mediaQuery = window.matchMedia('(max-width: 500)');
-		setIsMobile(mediaQuery.matches);
+		// Create a media query to detect mobile view (max-width: 638px)
+		const mediaQuery = window.matchMedia('(max-width: 638px)');
+		setIsMobile(mediaQuery.matches); // Set the initial state based on the media query result
 
+		// Define a callback function to handle changes in the media query
 		const handleMediaQueryChange = (event) => {
-			setIsMobile(event.matches);
+			setIsMobile(event.matches); // Update the state when the media query changes
 		};
+
+		// Add the callback function as a event listener for changes to the media query
 		mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+		// Clean up the event listener when the component unmounts
 		return () => {
 			mediaQuery.removeEventListener('change', handleMediaQueryChange);
 		};
-	}, []);
+	}, []); // The empty dependency array ensures this effect runs once, similar to componentDidMount
+
+	// Return a Canvas component to render the 3D scene
 	return (
 		<Canvas
 			frameloop='demand'
 			shadows
-			camera={{ position: [20, 3, 5], fov: 35 }}
+			camera={{ position: [20, 3, 5], fov: 35 }} // Set camera position and field of view
 			gl={{ preserveDrawingBuffer: true }}>
+			{' '}
+			{/* Set WebGL rendering options */}
 			<Suspense fallback={<CanvasLoader />}>
+				{' '}
+				{/* Display loading component while assets are loading */}
 				<OrbitControls
-					enableZoom={false}
-					maxPolarAngle={Math.PI / 2}
-					minPolarAngle={Math.PI / 2}
+					enableZoom={false} // Disable zooming with the mouse
+					maxPolarAngle={Math.PI / 2} // Set maximum polar angle for the camera
+					minPolarAngle={Math.PI / 2} // Set minimum polar angle for the camera
 				/>
-				<Computers isMobile={isMobile} />
+				<Computers isMobile={isMobile} /> {/* Render the 3D computer model */}
 			</Suspense>
-			<Preload all />
+			<Preload all /> {/* Preload all assets */}
 		</Canvas>
 	);
 };
 
-export default ComputersCanvas;
+export default ComputersCanvas; // Export the main component
